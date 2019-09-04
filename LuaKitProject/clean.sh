78b9d24293458d5763f1a59c3d00b08535f798ee
Larpoux <larpoux@canardoux.xyz>
2019-09-04 14:30:58 +0200

    .gitignore src/Projects/jni/CMakeFiles

--- LuaKitProject/clean.sh
+++ LuaKitProject/clean.sh
@@ -9,7 +9,7 @@ rm -rf src/Projects/libevent/CMakeFiles      src/Projects/libevent/outputs      
 rm -rf src/Projects/libiconv-1.14/CMakeFiles src/Projects/libiconv-1.14/outputs src/Projects/libiconv-1.14/CMakeCache.txt
 rm -rf src/Projects/libxml/CMakeFiles        src/Projects/libxml/outputs        src/Projects/libxml/CMakeCache.txt
 rm -rf src/Projects/modp_b64/CMakeFiles      src/Projects/modp_b64/outputs      src/Projects/modp_b64/CMakeCache.txt
-rm -rf src/Projects/jni/CMakefiles           src/Projects/jni/outputs           src/Projects/jni/CMakeCache.txt
+rm -rf src/Projects/jni/CMakeFiles           src/Projects/jni/outputs           src/Projects/jni/CMakeCache.txt
 #rm -rf src/Projects/openssl-1.1.1c/lib
 rm -rf AndroidDemo/AsyncSocketTest/app/.cxx     AndroidDemo/AsyncSocketTest/app/.externalNativeBuild    AndroidDemo/AsyncSocketTest/app/build
 rm -rf AndroidDemo/NotificationTest/app/.cxx    AndroidDemo/NotificationTest/app/.externalNativeBuild   AndroidDemo/NotificationTest/app/build
