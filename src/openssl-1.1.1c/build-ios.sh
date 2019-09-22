d879a1e706cdf7d46c5a2ccda32aeee6c47b4172
Larpoux <larpoux@canardoux.xyz>
2019-09-22 19:08:02 +0200

    Cleaning build-macos.sh during openssl

--- src/openssl-1.1.1c/build-ios.sh
+++ src/openssl-1.1.1c/build-ios.sh
@@ -33,46 +33,46 @@ buildIOS()
 {
 	ARCH=$1
     	OPENSSL_CONFIGURE_OPTIONS="no-whirlpool no-ui no-engine -fPIC"
-	if [[ "${ARCH}" == "i386" || "${ARCH}" == "x86_64" ]]; then
+	if [[ "$ARCH" == "i386" || "$ARCH" == "x86_64" ]]; then
 		PLATFORM="iPhoneSimulator"
 	else
 		PLATFORM="iPhoneOS"
 	fi
   
 	export $PLATFORM
-   	export CROSS_TOP="${DEVELOPER}/Platforms/${PLATFORM}.platform/Developer"
-	export CROSS_SDK="${PLATFORM}${IOS_SDK_VERSION}.sdk"
-	export BUILD_TOOLS="${DEVELOPER}"
-	export CC="${BUILD_TOOLS}/usr/bin/gcc -arch ${ARCH}"
-    	mkdir -p "${LIB_ROOT}/ios$IOS_SDK_VERSION-$CONFIG/${ARCH}"
+   	export CROSS_TOP="$DEVELOPER/Platforms/$PLATFORM.platform/Developer"
+	export CROSS_SDK="$PLATFORM$IOS_SDK_VERSION.sdk"
+	export BUILD_TOOLS="$DEVELOPER"
+	export CC="$BUILD_TOOLS/usr/bin/gcc -arch $ARCH"
+    	mkdir -p "$LIB_ROOT/ios$IOS_SDK_VERSION-$CONFIG/$ARCH"
 
-	echo "Building ${OPENSSL_VERSION} for ${PLATFORM}  ${IOS_SDK_VERSION} ${ARCH}"
+	echo "Building $OPENSSL_VERSION for $PLATFORM  $IOS_SDK_VERSION $ARCH"
 
-	if [[ "${ARCH}" == "x86_64" ]]; then
-		./Configure darwin64-x86_64-cc   ${OPENSSL_CONFIGURE_OPTIONS}
+	if [[ "$ARCH" == "x86_64" ]]; then
+		./Configure darwin64-x86_64-cc   $OPENSSL_CONFIGURE_OPTIONS
 	else
-		./Configure iphoneos-cross  ${OPENSSL_CONFIGURE_OPTIONS}
+		./Configure iphoneos-cross  $OPENSSL_CONFIGURE_OPTIONS
 	fi
 	if [ $? -ne 0 ]; then
-		echo "Error executing:./Configure ${ARCH}  ${OPENSSL_CONFIGURE_OPTIONS}"
+		echo "Error executing:./Configure $ARCH  $OPENSSL_CONFIGURE_OPTIONS"
 		exit 1
 	fi
-	sed -ie "s!^CFLAG=!CFLAG=-isysroot ${CROSS_TOP}/SDKs/${CROSS_SDK} -miphoneos-version-min=${IOS_SDK_VERSION} !" "Makefile"
+	sed -ie "s!^CFLAG=!CFLAG=-isysroot $CROSS_TOP/SDKs/$CROSS_SDK -miphoneos-version-min=$IOS_SDK_VERSION !" "Makefile"
 
 	make clean
 	make -j4 
 	if [ $? -ne 0 ]; then
-		echo "Error executing make for platform:${ARCH}"
+		echo "Error executing make for platform:$ARCH"
 		exit 1
 	fi
-	cp -v libcrypto.a "${LIB_ROOT}/ios$IOS_SDK_VERSION-$CONFIG/${ARCH}"
-	cp -v libssl.a "${LIB_ROOT}/ios$IOS_SDK_VERSION-$CONFIG/${ARCH}"
-	cp -v libcrypto.so.1.1 "${LIB_ROOT}/ios$IOS_SDK_VERSION-$CONFIG/${ARCH}/libcrypto.1.1.so"
-	cp -v libssl.so.1.1 "${LIB_ROOT}/ios$IOS_SDK_VERSION-$CONFIG/${ARCH}/libssl.1.1.so"
+	cp -v libcrypto.a "$LIB_ROOT/ios$IOS_SDK_VERSION-$CONFIG/$ARCH"
+	cp -v libssl.a "$LIB_ROOT/ios$IOS_SDK_VERSION-$CONFIG/$ARCH"
+	cp -v libcrypto.so.1.1 "$LIB_ROOT/ios$IOS_SDK_VERSION-$CONFIG/$ARCH/libcrypto.1.1.so"
+	cp -v libssl.so.1.1 "$LIB_ROOT/ios$IOS_SDK_VERSION-$CONFIG/$ARCH/libssl.1.1.so"
 
 	# copy header
-	mkdir -p "$OUTPUT_DIR/include-${ARCH}/openssl"
-	cp -r -v "include/openssl" "$OUTPUT_DIR/include-${ARCH}/"
+	mkdir -p "$OUTPUT_DIR/include-$ARCH/openssl"
+	cp -r -v "include/openssl" "$OUTPUT_DIR/include-$ARCH/"
 }
 
 
@@ -82,18 +82,18 @@ buildIOS()
 
 echo "Building iOS libraries"
 lipo \
-	"${LIB_ROOT}/ios$IOS_SDK_VERSION-$CONFIG/arm64/libcrypto.a" \
-	"${LIB_ROOT}/ios$IOS_SDK_VERSION-$CONFIG/x86_64/libcrypto.a" \
+	"$LIB_ROOT/ios$IOS_SDK_VERSION-$CONFIG/arm64/libcrypto.a" \
+	"$LIB_ROOT/ios$IOS_SDK_VERSION-$CONFIG/x86_64/libcrypto.a" \
 	-create -output "$OUTPUT_DIR/libcrypto.a"
 
 lipo \
-	"${LIB_ROOT}/ios$IOS_SDK_VERSION-$CONFIG/arm64/libssl.a" \
-	"${LIB_ROOT}/ios$IOS_SDK_VERSION-$CONFIG/x86_64/libssl.a" \
+	"$LIB_ROOT/ios$IOS_SDK_VERSION-$CONFIG/arm64/libssl.a" \
+	"$LIB_ROOT/ios$IOS_SDK_VERSION-$CONFIG/x86_64/libssl.a" \
 	-create -output "$OUTPUT_DIR/libssl.a"
 
 make clean
     
-#cp -r "${LIB_ROOT}/ios$IOS_SDK_VERSION-$CONFIG/" "$OUTPUT_DIR"
+#cp -r "$LIB_ROOT/ios$IOS_SDK_VERSION-$CONFIG/" "$OUTPUT_DIR"
 echo
 echo "Your ouputs are in " "$OUTPUT_DIR"
 echo
--- src/openssl-1.1.1c/build-macos.sh
+++ src/openssl-1.1.1c/build-macos.sh
@@ -10,12 +10,10 @@ if [ -z $MACOS_SDK_VERSION ]; then
 fi
 
 
+LIB_ROOT=./generation
 
-LIB_ROOT=./
-
-DEFAULT_OUTPUT=../../libs/macos$MACOS_MACOS_SDK_VERSION-$CONFIG
+DEFAULT_OUTPUT=../../libs/macos$MACOS_SDK_VERSION-$CONFIG
 #--------------------------------------------------
-path=$(dirname "$0")
 
 #
 # Canonicalize relative paths to absolute paths
@@ -25,45 +23,41 @@ then
     mkdir -p "$DEFAULT_OUTPUT" 2>/dev/null
     OUTPUT_DIR="$DEFAULT_OUTPUT"
 fi
-
 pushd "$OUTPUT_DIR" > /dev/null
 dir=$(pwd)
 OUTPUT_DIR="$dir"
 popd > /dev/null
 #OPENSSL_CONFIGURE_OPTIONS="no-whirlpool no-ui no-engine -fPIC"
-
-./Configure darwin64-x86_64-cc ${OPENSSL_CONFIGURE_OPTIONS}
+./Configure darwin64-x86_64-cc 
 if [ $? -ne 0 ]; then
-    echo "Error executing:./Configure ${CONFIGURE_ARCH} -D__ANDROID_API__=$ANDROID_API ${OPENSSL_CONFIGURE_OPTIONS}"
+    echo "Error executing: ./Configure darwin64-x86_64-cc $OPENSSL_CONFIGURE_OPTIONS"
     exit 1
 fi
-
      make clean
      make -j4
 
      if [ $? -ne 0 ]; then
-         echo "Error executing make for platform:${ANDROID_TARGET_PLATFORM}"
+         echo "Error executing make for platform:$ANDROID_TARGET_PLATFORM"
          exit 1
      fi
-   LIB_ROOT = ./generation 
-    mkdir -p "${LIB_ROOT}/macos-$CONFIG/"
+    mkdir -p "$LIB_ROOT/macos-$CONFIG/"
 
-    rm "${LIB_ROOT}/macos-$CONFIG/libcrypto.dylib" 2>/dev/null
-    mv libcrypto.1.1.dylib "${LIB_ROOT}/macos-$CONFIG/"
-    rm "${LIB_ROOT}/macos-$CONFIG/libcrypto.a" 2>/dev/null
-    mv libcrypto.a "${LIB_ROOT}/macos-$CONFIG/"
-    rm "${LIB_ROOT}/macos-$CONFIG/libssl.a" 2>/dev/null
-    mv libssl.a "${LIB_ROOT}/macos-$CONFIG/"
-    rm "${LIB_ROOT}/macos-$CONFIG/libssl.dylib" 2>/dev/null
-    mv libssl.1.1.dylib "${LIB_ROOT}/macos-$CONFIG/"
+    rm "$LIB_ROOT/macos-$CONFIG/libcrypto.dylib" 2>/dev/null
+    mv libcrypto.1.1.dylib "$LIB_ROOT/macos-$CONFIG/"
+    rm "$LIB_ROOT/macos-$CONFIG/libcrypto.a" 2>/dev/null
+    mv libcrypto.a "$LIB_ROOT/macos-$CONFIG/"
+    rm "$LIB_ROOT/macos-$CONFIG/libssl.a" 2>/dev/null
+    mv libssl.a "$LIB_ROOT/macos-$CONFIG/"
+    rm "$LIB_ROOT/macos-$CONFIG/libssl.dylib" 2>/dev/null
+    mv libssl.1.1.dylib "$LIB_ROOT/macos-$CONFIG/"
 
      # copy header
-     mkdir -p "${LIB_ROOT}/macos-$CONFIG/include-x86_64/openssl-x86_64"
-     cp -v -r "include/openssl" "${LIB_ROOT}/macos-$CONFIG/include-x86_64/"
+     mkdir -p "$LIB_ROOT/macos-$CONFIG/include-x86_64/openssl-x86_64"
+     cp -v -r "include/openssl" "$LIB_ROOT/macos-$CONFIG/include-x86_64/"
 
     make clean
 
-cp -v -r "${LIB_ROOT}/macos-$CONFIG/" "$OUTPUT_DIR"
+cp -v -r "$LIB_ROOT/macos-$CONFIG/" "$OUTPUT_DIR"
 echo
 echo "Your ouputs are in " "$OUTPUT_DIR"
 echo
