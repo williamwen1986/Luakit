8d3c10924dcf2a0f4dc40d66fecd930f7df320e0
Larpoux <larpoux@canardoux.xyz>
2019-10-20 09:02:56 +0200

    Working on Linux port

--- LinuxFramework/dummy.cpp
+++ LinuxFramework/dummy.cpp
@@ -1,4 +1,1 @@
-#include "JniEnvWrapper.h"
-#include "com_common_luakit_LuaHelper.cpp"
-
-static JniEnvWrapper* dummy; // just to satisfy this stupid android linker
+static void* dummy; // just to satisfy this stupid android linker
